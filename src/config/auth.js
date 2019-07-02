212d118c5894e314960471fc547eb44d2e65c260
murilokrugner <murilokrugner@hotmail.com>
2019-07-01 23:07:10 -0300

    feature: create user, update user, autentication user and validate user

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ba24561772f06bc3b5c8487325987720',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,27 @@
 // importando router para roteamento das rotas
 import { Router } from 'express';
 
-// importando models User/
-import User from './app/models/User';
+// importando middleware
+import authMiddleware from './app/middlewares/auth';
+
+// importando constrollers
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 // invocando o router
 const routes = new Router();
 
-// rota de teste
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Murilo Krugner',
-    email: 'murilokrugner@hotmail.com',
-    password_hash: '123456789',
-  });
+// rota para criar usuario
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
+// rota para fazer login de usuario
+routes.post('/sessions', SessionController.store);
+
+// definindo middleware global
+routes.use(authMiddleware);
+
+// rota para alterar dados usuario
+routes.put('/users', UserController.update);
 
 // exportando configurações
 export default routes;
