2979dd33f6c801002d8676e142de77c6146a4c73
Murilo Krugner <murilokrugner@hotmail.com>
2019-07-11 22:04:59 -0300

    feature: Queue configuration with Redis

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,37 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  // é chamado para o envio de cada e-mail
+  async handle({ data }) {
+    const { appointment } = data;
+
+    // teste
+    console.log('A fila executou!');
+
+    // enviar email de cancelamento
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      content: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
